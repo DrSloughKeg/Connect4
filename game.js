d0f53c855270c1599f67e87c5d66f3532b2bcb8f
Mikepin23 <137085441+Mikepin23@users.noreply.github.com>
2023-09-17 22:06:18 -0400

    updated the game a bit to make it similar to connect 4

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 //set up the board
-function makeBoard(size) {
+function makeBoard(row, column) {
   let newTable = document.createElement("table");
 
-  for (r = 1; r <= size; r++) {
+  for (r = 1; r <= row; r++) {
     let newRows = document.createElement("tr");
     newTable.appendChild(newRows);
-    for (c = 1; c <= size; c++) {
+    for (c = 1; c <= column; c++) {
       let newCols = document.createElement("td");
       newCols.setAttribute("id", r + "," + c);
       newRows.appendChild(newCols);
@@ -14,42 +14,70 @@ function makeBoard(size) {
   document.querySelector("body").appendChild(newTable);
 }
 
-function placeTile(x, y) {
-  let selectedTile = document.getElementById(x + "," + y);
-  selectedTile.style.backgroundColor = "black";
+makeBoard(6, 7);
+
+function placeTile(x, y, color) {
+  for (let r = 6; r >= 1; r--) {
+    let rowTile = document.getElementById(r + "," + y);
+    if (
+      rowTile.style.backgroundColor !== "black" &&
+      rowTile.style.backgroundColor !== "red"
+    ) {
+      rowTile.style.backgroundColor = color;
+      break;
+    }
+  }
 }
 
 function startPlayerMovement() {
-  let playerTile = document.getElementById("1,1");
-  let playerX = 1;
-  let playerY = 1;
-  playerTile.style.backgroundColor = "red";
+  let player1Tile = document.getElementById("1,1");
+  let player2Tile = document.getElementById("1,1");
+  let player1X = 1;
+  let player1Y = 1;
+  let player2X = 1;
+  let player2Y = 1;
+  player1Tile.style.backgroundColor = "black";
+  player2Tile.style.backgroundColor = "red";
+  let currentPlayer = 1;
   document.onkeydown = function (e) {
     switch (e.keyCode) {
-      case 37:
-        playerTile.style.backgroundColor = "white";
-        playerY--;
-        playerTile = document.getElementById(playerX + "," + playerY);
-        playerTile.style.backgroundColor = "red";
+      case 32:
+        if (currentPlayer == 1) {
+          placeTile(player1X, player1Y, "black");
+          currentPlayer = 2;
+        } else {
+          placeTile(player2X, player2Y, "red");
+          currentPlayer = 1;
+        }
         break;
-      case 38:
-        playerTile.style.backgroundColor = "white";
-        playerX--;
-        playerTile = document.getElementById(playerX + "," + playerY);
-        playerTile.style.backgroundColor = "red";
+      case 37:
+        if (currentPlayer == 1) {
+          player1Tile.style.backgroundColor = "white";
+          player1Y--;
+          player1Tile = document.getElementById(player1X + "," + player1Y);
+          player1Tile.style.backgroundColor = "black";
+        } else {
+          player2Tile.style.backgroundColor = "white";
+          player2Y--;
+          player2Tile = document.getElementById(player2X + "," + player2Y);
+          player2Tile.style.backgroundColor = "red";
+        }
         break;
       case 39:
-        playerTile.style.backgroundColor = "white";
-        playerY++;
-        playerTile = document.getElementById(playerX + "," + playerY);
-        playerTile.style.backgroundColor = "red";
-        break;
-      case 40:
-        playerTile.style.backgroundColor = "white";
-        playerX++;
-        playerTile = document.getElementById(playerX + "," + playerY);
-        playerTile.style.backgroundColor = "red";
+        if (currentPlayer == 1) {
+          player1Tile.style.backgroundColor = "white";
+          player1Y++;
+          player1Tile = document.getElementById(player1X + "," + player1Y);
+          player1Tile.style.backgroundColor = "black";
+        } else {
+          player2Tile.style.backgroundColor = "white";
+          player2Y++;
+          player2Tile = document.getElementById(player2X + "," + player2Y);
+          player2Tile.style.backgroundColor = "red";
+        }
         break;
     }
   };
 }
+
+startPlayerMovement();
