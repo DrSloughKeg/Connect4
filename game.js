de9eb6e4c3cbf590de51aec28945c7c8c4c18c9e
Mikepin23 <137085441+Mikepin23@users.noreply.github.com>
2023-09-18 16:21:35 -0400

    boundaries fixed, trying to fix floating chips

--- game.js
+++ game.js
@@ -4,6 +4,10 @@ function makeBoard(row, column) {
 
   for (r = 1; r <= row; r++) {
     let newRows = document.createElement("tr");
+    if (r === 1) {
+      // Add a class to the first row
+      newRows.classList.add("ignoreCSSRow");
+    }
     newTable.appendChild(newRows);
     for (c = 1; c <= column; c++) {
       let newCols = document.createElement("td");
@@ -14,70 +18,166 @@ function makeBoard(row, column) {
   document.querySelector("body").appendChild(newTable);
 }
 
-makeBoard(6, 7);
+makeBoard(7, 7);
 
-function placeTile(x, y, color) {
-  for (let r = 6; r >= 1; r--) {
+function placeTile(x, y, color, player) {
+  for (let r = 7; r >= 1; r--) {
     let rowTile = document.getElementById(r + "," + y);
     if (
       rowTile.style.backgroundColor !== "black" &&
       rowTile.style.backgroundColor !== "red"
     ) {
       rowTile.style.backgroundColor = color;
+      rowTile.setAttribute("class", player);
+      checkWinCond(player);
       break;
     }
   }
 }
 
 function startPlayerMovement() {
+  // Setting Both player 1 and 2 at the first tile (1,1)
   let player1Tile = document.getElementById("1,1");
   let player2Tile = document.getElementById("1,1");
-  let player1X = 1;
-  let player1Y = 1;
-  let player2X = 1;
-  let player2Y = 1;
-  player1Tile.style.backgroundColor = "black";
-  player2Tile.style.backgroundColor = "red";
-  let currentPlayer = 1;
+  let player1R = 1;
+  let player1C = 1;
+  let player2R = 1;
+  let player2C = 1;
+  player1Tile.style.backgroundColor = "black"; // Player 1 is black & Player 2 is red
+  let currentPlayer = 1; // player 1 starts
+
   document.onkeydown = function (e) {
     switch (e.keyCode) {
-      case 32:
+      case 32: // When Space bar is pressed
         if (currentPlayer == 1) {
-          placeTile(player1X, player1Y, "black");
+          placeTile(player1R, player1C, "black", "player1");
+          player1Tile.style.backgroundColor = "white";
           currentPlayer = 2;
         } else {
-          placeTile(player2X, player2Y, "red");
+          placeTile(player2R, player2C, "red", "player2");
+          player2Tile.style.backgroundColor = "white";
           currentPlayer = 1;
         }
         break;
-      case 37:
-        if (currentPlayer == 1) {
+      case 37: // When left arrow is pressed
+        if (currentPlayer == 1 && player1C > 1) {
+          // (&& PLayer1C > 1) sets the boundary to the board.
           player1Tile.style.backgroundColor = "white";
-          player1Y--;
-          player1Tile = document.getElementById(player1X + "," + player1Y);
+          player1C--;
+          player1Tile = document.getElementById(player1R + "," + player1C);
           player1Tile.style.backgroundColor = "black";
-        } else {
+        } else if (currentPlayer == 2 && player2C > 1) {
           player2Tile.style.backgroundColor = "white";
-          player2Y--;
-          player2Tile = document.getElementById(player2X + "," + player2Y);
+          player2C--;
+          player2Tile = document.getElementById(player2R + "," + player2C);
           player2Tile.style.backgroundColor = "red";
         }
         break;
-      case 39:
-        if (currentPlayer == 1) {
+      case 39: // When Right arrow is pressed
+        if (currentPlayer == 1 && player1C < 7) {
           player1Tile.style.backgroundColor = "white";
-          player1Y++;
-          player1Tile = document.getElementById(player1X + "," + player1Y);
+          player1C++;
+          player1Tile = document.getElementById(player1R + "," + player1C);
           player1Tile.style.backgroundColor = "black";
-        } else {
+        } else if (currentPlayer == 2 && player2C < 7) {
           player2Tile.style.backgroundColor = "white";
-          player2Y++;
-          player2Tile = document.getElementById(player2X + "," + player2Y);
+          player2C++;
+          player2Tile = document.getElementById(player2R + "," + player2C);
           player2Tile.style.backgroundColor = "red";
         }
         break;
     }
   };
+}
+
+function checkWinCond(player) {
+  winCond = 0;
+  for (r = 1; r <= 6; r++) {
+    for (c = 1; c <= 7; c++) {
+      let rowTile = document.getElementById(r + "," + c);
+      if (rowTile.className == player) {
+        if (
+          document.getElementById(r + "," + (c + 1)) != null &&
+          document.getElementById(r + "," + (c + 1)).className == player
+        ) {
+          checkHori(r, c, rowTile, player);
+        }
+        if (
+          document.getElementById(r - 1 + "," + c) != null &&
+          document.getElementById(r - 1 + "," + c).className == player
+        ) {
+          checkVert(r, c, rowTile, player);
+        }
+        if (
+          document.getElementById(r - 1 + "," + (c + 1)) != null &&
+          document.getElementById(r - 1 + "," + (c + 1)).className == player
+        ) {
+          checkDiag1(r, c, rowTile, player);
+        }
+        if (
+          document.getElementById(r - 1 + "," + (c - 1)) != null &&
+          document.getElementById(r - 1 + "," + (c - 1)).className == player
+        ) {
+          checkDiag2(r, c, rowTile, player);
+        }
+      }
+    }
+  }
+}
+
+//check horizontal
+function checkHori(r, c, rowTile, player) {
+  winCond++;
+  for (i = 1; i < 4; i++) {
+    rowTile = document.getElementById(r + "," + (c + i));
+    if (rowTile != null && rowTile.className == player) {
+      winCond++;
+    }
+  }
+  winOrReset(player);
+}
+
+//check vertical
+function checkVert(r, c, rowTile, player) {
+  winCond++;
+  for (i = 1; i < 4; i++) {
+    rowTile = document.getElementById(r - i + "," + c);
+    if (rowTile != null && rowTile.className == player) {
+      winCond++;
+    }
+  }
+  winOrReset(player);
+}
+
+function checkDiag1(r, c, rowTile, player) {
+  winCond++;
+  for (i = 1; i < 4; i++) {
+    rowTile = document.getElementById(r - i + "," + (c + i));
+    if (rowTile != null && rowTile.className == player) {
+      winCond++;
+    }
+  }
+  winOrReset(player);
+}
+
+function checkDiag2(r, c, rowTile, player) {
+  winCond++;
+  for (i = 1; i < 4; i++) {
+    rowTile = document.getElementById(r - i + "," + (c - i));
+    if (rowTile != null && rowTile.className == player) {
+      winCond++;
+    }
+  }
+  winOrReset(player);
+}
+
+function winOrReset(player) {
+  if (winCond >= 4) {
+    //player wins
+    console.log(player + " Wins!");
+  } else {
+    winCond = 0;
+  }
 }
 
 startPlayerMovement();
